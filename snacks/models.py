3498a07d3538f4492905a7c0d3e88513fe72ecd2
dchoi294 <dchoi294@gmail.com>
2023-01-24 16:51:39 -0800

    lab27

--- snacks/models.py
+++ snacks/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth import get_user_model
 
 
 class Snack(models.Model):
-    name = models.CharField(max_length=256)
-    rating = models.IntegerField(default=0)
-    reviewer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=1000, default='null')
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
